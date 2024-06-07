54b8b612367855d3053ab8c526907f46224f392c
mirzaeimahdi409 <mirzaeimahdi409@gmail.com>
2024-06-07 22:34:52 +0330

    first commit
    (cherry picked from commit 8399c2e5fc2757987c910a538d1b4dd1af7e7784)

---
+++ src/test/java/com/dishdash/diashdash/DiashDashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dishdash.diashdash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiashDashApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
