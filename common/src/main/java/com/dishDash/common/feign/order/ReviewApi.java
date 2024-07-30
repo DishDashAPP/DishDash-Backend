ca269ede0c34e754038f906855ab1a3884d68088
mirzaeimahdi409 <mirzaeimahdi409@gmail.com>
2024-07-30 16:48:43 +0330

    add path to feign

--- common/src/main/java/com/dishDash/common/feign/order/ReviewApi.java
+++ common/src/main/java/com/dishDash/common/feign/order/ReviewApi.java
@@ -3,7 +3,7 @@ package com.dishDash.common.feign.order;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
-@FeignClient(name = "order-service", contextId = "review-order-service")
+@FeignClient(name = "order-service", contextId = "review-order-service", path = "/review")
 public interface ReviewApi {
   @PostMapping("/order")
   boolean setOrderReview(
