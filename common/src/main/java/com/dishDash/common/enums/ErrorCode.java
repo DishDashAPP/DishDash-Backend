255d51316ee680fcdb406ba5882717c54982d9a6
mirzaeimahdi409 <mirzaeimahdi409@gmail.com>
2024-08-02 23:53:19 +0330

    change error code

--- common/src/main/java/com/dishDash/common/enums/ErrorCode.java
+++ common/src/main/java/com/dishDash/common/enums/ErrorCode.java
@@ -7,22 +7,22 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum ErrorCode {
-  INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, 500, "Internal Server Error"),
-  SERVICE_UNAVAILABLE(HttpStatus.SERVICE_UNAVAILABLE, 503, "Service Unavailable"),
+  INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, 500, "Internal_server_error"),
+  SERVICE_UNAVAILABLE(HttpStatus.SERVICE_UNAVAILABLE, 503, "Service_unavailable"),
   UNAUTHORIZED(HttpStatus.UNAUTHORIZED, 401, "Unauthorized"),
   FORBIDDEN(HttpStatus.FORBIDDEN, 403, "Forbidden"),
-  INVALID_TOKEN(HttpStatus.FORBIDDEN, 403, "Invalid Token"),
-  BAD_REQUEST(HttpStatus.BAD_REQUEST, 400, "Bad Request"),
-  USER_ALREADY_EXISTS(HttpStatus.BAD_REQUEST, 400, "User already exists"),
-  NO_CONTENT(HttpStatus.NO_CONTENT, 204, "No Content"),
-  INVALID_CREDENTIALS(HttpStatus.UNAUTHORIZED, 401, "Invalid credentials"),
-  NOT_FOUND(HttpStatus.NOT_FOUND, 404, "Resource not found"),
+  INVALID_TOKEN(HttpStatus.FORBIDDEN, 403, "Invalid_token"),
+  BAD_REQUEST(HttpStatus.BAD_REQUEST, 400, "Bad_request"),
+  USER_ALREADY_EXISTS(HttpStatus.BAD_REQUEST, 400, "User_already_exists"),
+  NO_CONTENT(HttpStatus.NO_CONTENT, 204, "No_content"),
+  INVALID_CREDENTIALS(HttpStatus.UNAUTHORIZED, 401, "Invalid_credentials"),
+  NOT_FOUND(HttpStatus.NOT_FOUND, 404, "Resource_not_found"),
   CONFLICT(HttpStatus.CONFLICT, 409, "Conflict"),
-  UNPROCESSABLE_ENTITY(HttpStatus.UNPROCESSABLE_ENTITY, 422, "Unprocessable entity"),
-  PAYMENT_REQUIRED(HttpStatus.PAYMENT_REQUIRED, 402, "Payment required"),
-  METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, 405, "Method not allowed"),
-  UNSUPPORTED_MEDIA_TYPE(HttpStatus.UNSUPPORTED_MEDIA_TYPE, 415, "Unsupported media type"),
-  TOO_MANY_REQUESTS(HttpStatus.TOO_MANY_REQUESTS, 429, "Too many requests");
+  UNPROCESSABLE_ENTITY(HttpStatus.UNPROCESSABLE_ENTITY, 422, "Unprocessable_entity"),
+  PAYMENT_REQUIRED(HttpStatus.PAYMENT_REQUIRED, 402, "Payment_required"),
+  METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, 405, "Method_not_allowed"),
+  UNSUPPORTED_MEDIA_TYPE(HttpStatus.UNSUPPORTED_MEDIA_TYPE, 415, "Unsupported_media_type"),
+  TOO_MANY_REQUESTS(HttpStatus.TOO_MANY_REQUESTS, 429, "Too_many_requests");
 
   private final HttpStatus httpStatus;
   private final Integer errorCodeValue;
