2b4666ae96dc5aa79b73e787b6842e491485c151
Amirhossein Arabzadeh <arabzadehamirhossein888@gmail.com>
2024-08-02 21:38:28 +0330

    add payment and fix create order

--- common/src/main/java/com/dishDash/common/feign/payment/PaymentApi.java
+++ common/src/main/java/com/dishDash/common/feign/payment/PaymentApi.java
@@ -5,11 +5,11 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "payment-service")
+@FeignClient(name = "payment-service", contextId = "payment-service", path = "/payment")
 public interface PaymentApi {
   @PostMapping("/pay")
   boolean pay(@RequestParam String transactionID);
 
   @PostMapping("/createOrderTransaction")
-  TransactionDto createOrderTransaction(@RequestParam long orderId, @RequestParam double amount);
+  TransactionDto createOrderTransaction(@RequestParam Long shoppingCartId, @RequestParam double amount);
 }
