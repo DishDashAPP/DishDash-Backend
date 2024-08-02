2d2832864e11b670e35fad3035c25aa33d0c2761
mirzaeimahdi409 <mirzaeimahdi409@gmail.com>
2024-08-03 02:30:53 +0330

    add logs

--- gateway/src/main/java/com/dish_dash/gateway/adapters/CategoryController.java
+++ gateway/src/main/java/com/dish_dash/gateway/adapters/CategoryController.java
@@ -6,17 +6,20 @@ import com.dishDash.common.feign.Product.CategoryApi;
 import com.dish_dash.gateway.annotation.Authentication;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/v1/category")
 @RequiredArgsConstructor
+@Slf4j
 public class CategoryController {
   private final CategoryApi categoryApi;
 
   @GetMapping
   @Authentication(userId = "#userId")
   public List<CategoryViewDto> getAllCategories(Long userId) {
+    log.info("getAllCategories, userId: {}", userId);
     return categoryApi.getAllCategories(userId);
   }
 
