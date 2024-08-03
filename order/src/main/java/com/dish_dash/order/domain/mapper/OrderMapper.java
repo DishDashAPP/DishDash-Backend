d9d9470e8948652e39533459c6d0e5b2a935d856
mirzaeimahdi409 <mirzaeimahdi409@gmail.com>
2024-08-03 19:23:22 +0330

    pass test

--- order/src/main/java/com/dish_dash/order/domain/mapper/OrderMapper.java
+++ order/src/main/java/com/dish_dash/order/domain/mapper/OrderMapper.java
@@ -3,11 +3,9 @@ package com.dish_dash.order.domain.mapper;
 import com.dishDash.common.dto.*;
 import com.dish_dash.order.domain.model.Order;
 import com.dish_dash.order.domain.model.OrderItem;
-import com.dish_dash.order.domain.model.Rate;
+import java.util.List;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
-
-import java.util.List;
 
 @Mapper
 public interface OrderMapper {
@@ -18,15 +16,12 @@ public interface OrderMapper {
   Order orderDtoToOrder(OrderDto orderDto);
 
   OrderItem orderItemDtoToOrderItem(OrderItemDto orderItemDto);
+
   OrderItem orderItemCreateDtoToOrderItem(OrderItemCreateDto orderItemDto);
 
   OrderItem orderItemCreatenDtoToOrderItem(OrderItemCreateDto orderItemDto);
 
-  RateDto rateToRateDto(Rate rate);
-
   default double calculateTotalPrice(List<OrderItem> orderItems) {
-    return orderItems.stream()
-            .mapToDouble(orderItem -> orderItem.getPrice().getAmount())
-            .sum();
+    return orderItems.stream().mapToDouble(orderItem -> orderItem.getPrice().getAmount()).sum();
   }
 }
