d1b551eb22ff9783e801fac56d2dc589425fc6cf
Amirhossein Arabzadeh <arabzadehamirhossein888@gmail.com>
2024-08-03 11:32:58 +0330

    fix returning delivery person current order while it is null and creating menu if not exists in category creation

--- order/src/main/java/com/dish_dash/order/OrderApplication.java
+++ order/src/main/java/com/dish_dash/order/OrderApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.dish_dash.order", "com.dishDash.common"})
 @EnableEurekaClient
 @EnableFeignClients(basePackages = "com.dishDash.common.feign")
 public class OrderApplication {
